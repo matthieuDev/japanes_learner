1b77c31232b3ac454da273d79ce4380f80e253b0
matthieuDev <matthieu.devaux.epfl@outlook.com>
2023-05-16 02:41:14 +0200

    load categories letters

--- image_letter_classifier/generate_model.py
+++ image_letter_classifier/generate_model.py
@@ -1,3 +1,4 @@
+import csv
 import numpy as np
 import tensorflow as tf
 
@@ -53,6 +54,13 @@ def generate_model(save_file='model.h5'):
 
 def load_model(load_file='model.h5'):
     return tf.keras.models.load_model(load_file)
+
+def load_categories():
+    with open('k49_classmap.csv', encoding='utf8') as f :
+        csv_characters = csv.reader(f, delimiter=',')
+        next(csv_characters)
+        return np.array([symbol for _, _, symbol in csv_characters])
+    
 
 if __name__ == '__main__' :
     generate_model()
