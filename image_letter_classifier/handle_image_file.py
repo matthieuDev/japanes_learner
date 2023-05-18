3a7b5caba5ff96628e5821db465adabf137e4330
matthieuDev <matthieu.devaux.epfl@outlook.com>
2023-05-18 23:53:31 +0200

    better rescale

--- image_letter_classifier/handle_image_file.py
+++ image_letter_classifier/handle_image_file.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
+from math import floor , ceil
+
 def load_image(image_file, has_white_background = True) :
     '''
     The training set is white on black. If the given image is black on white, use has_white_background to switch the white and black
@@ -34,7 +36,7 @@ def clean_image(img):
     
     '''
     size_clean = 28
-    clean_image = np.zeros((size_clean, size_clean), dtype=int)
+    clean_image = np.zeros((size_clean, size_clean), dtype=float)
     
     if (clean_image.shape == img.shape) :
         return img
@@ -47,17 +49,51 @@ def clean_image(img):
     start_column, end_column = get_start_stop_true (column_ok)
 
     img = img[start_line: end_line, start_column: end_column]
-
+    
     i_step = img.shape[0] / size_clean
     j_step = img.shape[1] / size_clean
-
+    
     for i_clean, i_img in enumerate(np.arange(0, img.shape[0], i_step)) :
-        for j_clean, j_img in enumerate(np.arange(0, img.shape[1], j_step)) :
-            clean_image[i_clean, j_clean] = np.mean(img[round(i_img): round(i_img + i_step), round(j_img): round(j_img + j_step)], dtype=int)
-            '''clean_image[i_clean, j_clean] = (
-                np.mean(img[round(i_img): round(i_img + i_step), round(j_img): round(j_img + j_step)], dtype=int)
-            ) / (i_step * j_step)'''
-          
+        i_img_ceil = ceil(i_img)
+        i_img_up_decimal_number = i_img_ceil - i_img
+
+        i_next_img = i_img + i_step
+        next_i_img_floor = floor(i_next_img)
+        next_i_img_decimal_number = i_next_img - next_i_img_floor
+        for j_clean, j_img in enumerate(np.arange(0, img.shape[1], j_step)) :            
+            j_img_ceil = ceil(j_img)
+            j_img_up_decimal_number = j_img_ceil - j_img
+
+            j_next_img = j_img + j_step
+            next_j_img_floor = floor(j_next_img)
+            next_j_img_decimal_number = j_next_img - next_j_img_floor
+            
+            clean_image[i_clean, j_clean] = (
+                np.sum(img[i_img_ceil: next_i_img_floor, j_img_ceil: next_j_img_floor], dtype=int) +
+                
+                np.sum(img[floor(i_img), j_img_ceil: next_j_img_floor]) * i_img_up_decimal_number +
+                np.sum(img[i_img_ceil: next_i_img_floor, floor(j_img)]) * j_img_up_decimal_number +
+
+                img[floor(i_img), floor(j_img)] * i_img_up_decimal_number * j_img_up_decimal_number 
+            )
+            
+            if next_i_img_decimal_number :
+                clean_image[i_clean, j_clean] += (
+                    np.sum(img[next_i_img_floor, j_img_ceil: next_j_img_floor]) * next_i_img_decimal_number +
+                    img[next_i_img_floor, floor(j_img)] * next_i_img_decimal_number * j_img_up_decimal_number
+                )
+            
+            if next_j_img_decimal_number :
+                clean_image[i_clean, j_clean] += (
+                    np.sum(img[i_img_ceil: next_i_img_floor, next_j_img_floor]) * next_j_img_decimal_number +
+                    img[floor(i_img), next_j_img_floor] * i_img_up_decimal_number * next_j_img_decimal_number
+                )
+                if next_i_img_decimal_number :
+                    clean_image[i_clean, j_clean] +=\
+                        img[next_i_img_floor, next_j_img_floor] * next_i_img_decimal_number * next_j_img_decimal_number
+            
+    clean_image = np.round(clean_image / (i_step * j_step))
+
     return clean_image
 
 def print_img (img_arr):
