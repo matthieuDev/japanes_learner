8b8e90da38044cc7b3ff765422f194f905910bfe
matthieuDev <matthieu.devaux.epfl@outlook.com>
2023-05-31 17:21:09 +0200

    send image to backend

--- japanese_learner_site/latin2japanese/urls.py
+++ japanese_learner_site/latin2japanese/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.latin2japanese_question, name="latin2japanese_question"),
+    path("check/", views.get_img_letter, name="get_img_letter"),
 ]
--- japanese_learner_site/latin2japanese/views.py
+++ japanese_learner_site/latin2japanese/views.py
@@ -1,7 +1,14 @@
+import base64, io, json
+
+import matplotlib.image as mpimg
+
 from django.http import HttpResponse
 from django.template import loader
 
+from japanese_learner.image_letter_classifier.model_handler import model_handler
 from .models import QuestionLatin2japanese
+
+md = model_handler()
 
 def latin2japanese_question(request) :
     question = QuestionLatin2japanese(latin_text = 'a', japanese_text= "\u3042")
@@ -10,3 +17,18 @@ def latin2japanese_question(request) :
     template = loader.get_template("latin2japanese/questions.html")
 
     return HttpResponse(template.render(context, request))
+
+def get_img_letter(request) :
+    img = json.loads(request.body.decode('utf8'))['img']
+
+    assert img.startswith('data:image/png;base64,')
+    img = img.replace('data:image/png;base64,', '')
+
+    img = base64.b64decode(img)
+    img = io.BytesIO(img)
+    img = mpimg.imread(img, format='JPG')[:,:,3] * 255
+
+    res = md.predict(img)
+    print(res)
+
+    return res
