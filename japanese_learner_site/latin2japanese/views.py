5abb8c83a2bd1d49492c58ccc29b98030ea2913b
matthieuDev <matthieu.devaux.epfl@outlook.com>
2023-06-01 02:14:45 +0200

    add reset

--- japanese_learner_site/latin2japanese/views.py
+++ japanese_learner_site/latin2japanese/views.py
@@ -1,4 +1,4 @@
-import base64, io, json
+import base64, io, json, os
 
 import matplotlib.image as mpimg
 
@@ -9,6 +9,8 @@ from django.http.response import HttpResponse
 from japanese_learner.image_letter_classifier.model_handler import model_handler
 from .models import QuestionLatin2japanese
 
+#set to disable GPU for dev
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 md = model_handler()
 
 def latin2japanese_question(request) :
@@ -27,9 +29,9 @@ def get_img_letter(request) :
 
     img = base64.b64decode(img)
     img = io.BytesIO(img)
-    img = mpimg.imread(img, format='JPG')[:,:,3] * 255
+    img = mpimg.imread(img, format='png')
+    img = img[:,:,3] * 255
 
     res = str(md.predict(img))
-    print(res)
 
     return HttpResponse(res)
